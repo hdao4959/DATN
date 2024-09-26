a3c54371c3762ad39e2134d1fed4f3fa87fb4bb8
ryanhung044 <hunglsb2004@gmail.com>
2024-09-26 22:40:38 +0700

    comment db

--- BE/database/migrations/2024_09_24_164514_create_class_rooms_table.php
+++ BE/database/migrations/2024_09_24_164514_create_class_rooms_table.php
@@ -15,20 +15,20 @@ return new class extends Migration
     {
         Schema::create('class_rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('classCode',255)->unique();
-            $table->string('className',255);
-            $table->text('examScore',5000);
-            $table->text('schoolSchedule',5000);
-            $table->text('examSchdule',5000);
-            $table->text('description',5000);
-            $table->date('dateFrom');
-            $table->date('dateTo');
-            $table->text('student',5000);
+            $table->string('classCode',255)->unique()->comment('Mã lớp');
+            $table->string('className',255)->comment('Tên lớp');
+            $table->text('examScore',5000)->comment('Json điểm thi');
+            $table->text('schoolSchedule',5000)->comment('Json lịch học');
+            $table->text('examSchdule',5000)->comment('Json lịch thi');
+            $table->text('description',5000)->comment('Mô tả');
+            $table->date('dateFrom')->comment('Từ ngày');
+            $table->date('dateTo')->comment('Đến ngày');
+            $table->text('student',5000)->comment('Json sinh viên');
             $table->boolean('isActive');
             $table->boolean('isDelete');
-            $table->string('studyRoomCode',255);
-            $table->string('subjectCode',255);
-            $table->foreignId('userCode')->constrained();
+            $table->string('studyRoomCode',255)->comment('Phòng học');
+            $table->string('subjectCode',255)->comment('Môn học');
+            $table->foreignId('userCode')->constrained()->comment('Giáo viên');
             $table->timestamps();
         });
     }
