50361b43d8b4757177f620d9e4ffdc6563095605
hoanganhdzvcl <hoanganhdz2004@gmail.com>
2024-09-30 22:54:58 +0700

    Call api signin

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -5,12 +5,17 @@ import Dashboard from "./pages/admin/Dashboard/Dashboard";
 import MajorList from "./pages/admin/Major/MajorList";
 import AddMajor from "./pages/admin/Major/AddMajor";
 import EditMajor from "./pages/admin/Major/EditMajor";
+import Signin from "./pages/admin/Auth/Signin";
 
 function App() {
     const router = createBrowserRouter([
         {
             path: "",
             element: <h1>Hello World!</h1>,
+        },
+        {
+            path: "/signin",
+            element: <Signin />,
         },
         {
             path: "admin",
