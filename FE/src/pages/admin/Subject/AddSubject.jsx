5c137feecb8e27005e0d8db37480b9d445ceca76
Trung2310 <zrfixh@gmail.com>
2024-10-02 19:30:56 +0700

    update Subjects Management UI

--- FE/src/pages/admin/Subject/AddSubject.jsx
+++ FE/src/pages/admin/Subject/AddSubject.jsx
@@ -1,157 +1,112 @@
-import React, { useState } from 'react'
-import { Form, Input, InputNumber, Select, Button, message, Radio } from 'antd'
-import axios from 'axios'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
-const { Option } = Select
+const AddSubject = ({ handleClose }) => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
-const AddSubject = () => {
-  const [form] = Form.useForm() 
-
-  const onFinish = async (values) => {
+  const onSubmitForm = async (data) => {
     try {
-      const response = await axios.post('/http://localhost:8000/api/admin/subjects', values);
-      message.success('Thêm môn học thành công!')
-      form.resetFields()
+      await axios.post('http://localhost:8000/api/admin/subjects', data);
+      alert('Môn học đã được thêm thành công!');
+      reset();
+      handleClose();
     } catch (error) {
-      message.error('Có lỗi xảy ra, vui lòng thử lại!')
+      console.error('Lỗi khi thêm môn học:', error);
+      alert('Có lỗi xảy ra khi thêm môn học!');
     }
-  }
+  };
 
   return (
     <>
-      <h2>
-        <i className='fas fa-plus'>Thêm Môn học mới</i>
-      </h2>
-      <Form
-        layout="vertical"
-        form={form}
-        onFinish={onFinish}
-        initialValues={{
-          isActive: 1,
-        }}
-      >
-        <div className="row">
-          <div className="col-md-6 col-lg-6">
-            <Form.Item
-              label="Tên môn học"
-              name="subjectName"
-              rules={[{ required: true, message: 'Vui lòng nhập tên môn học!' }]}
-            >
-              <Input placeholder="Nhập tên môn học" />
-            </Form.Item>
-
-            <Form.Item
-              label="Mã môn"
-              name="subjectCode"
-              rules={[{ required: true, message: 'Vui lòng nhập mã môn!' }]}
-            >
-              <Input placeholder="Nhập mã môn học" />
-            </Form.Item>
-
-            <Form.Item
-              label="Số tín chỉ"
-              name="creditNumber"
-              rules={[{ required: true, message: 'Vui lòng nhập số tín chỉ!' }]}
-            >
-              <InputNumber min={1} placeholder="Nhập số tín chỉ" style={{ width: '100%' }} />
-            </Form.Item>
-
-            <Form.Item
-              label="Học phí"
-              name="tuition"
-              rules={[{ required: true, message: 'Vui lòng nhập học phí!' }]}
-            >
-              <InputNumber
-                min={0}
-                placeholder="Nhập học phí"
-                style={{ width: '100%' }}
-                formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
-                parser={value => value.replace(/\./g, '')}
-                addonAfter="VND"
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Phí học lại"
-              name="reStudyFee"
-              rules={[{ required: true, message: 'Vui lòng nhập phí học lại!' }]}
-            >
-              <InputNumber
-                min={0}
-                placeholder="Nhập phí học lại"
-                style={{ width: '100%' }}
-                formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
-                parser={value => value.replace(/\./g, '')}
-                addonAfter="VND"
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Giới thiệu môn học"
-              name="description"
-            >
-              <Input.TextArea rows={5} placeholder="Nhập giới thiệu môn học" />
-            </Form.Item>
+      <div className="modal-header d-flex justify-content-between align-items-center">
+                <h5 className="modal-title flex-grow-1 text-center">Thêm Môn Học</h5>
+                <button type="button" className="close" onClick={handleClose}>
+                    <div className="bg-red-500" style={{ padding: '10px', borderRadius: '20%'}}>
+                        <span>&times;</span>
+                    </div>
+                </button>
+            </div>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
+        <div className="modal-body">
+          <div className="form-group">
+            <label>Mã Môn:</label>
+            <input
+              type="text"
+              className="form-control"
+              {...register('subjectCode', { required: 'Mã môn không được để trống.' })}
+            />
+            {errors.subjectCode && <span className="text-danger">{errors.subjectCode.message}</span>}
           </div>
-
-          <div className="col-md-6 col-lg-6">
-            <Form.Item
-              label="Số buổi học"
-              name="examDay"
-              rules={[{ required: true, message: 'Vui lòng nhập số buổi học' }]}
-            >
-              <InputNumber min={1} style={{ width: '100%' }} placeholder="Nhập số buổi học" />
-            </Form.Item>
-            <Form.Item
-              label="Học kì"
-              name="semestersCode"
-              rules={[{ required: true, message: 'Vui lòng chọn học kì!' }]}
-            >
-              <Select placeholder="Chọn học kì">
-                {/* /// */}
-              </Select>
-            </Form.Item>
-
-            <Form.Item
-              label="Chuyên ngành"
-              name="majorCode"
-              rules={[{ required: true, message: 'Vui lòng chọn chuyên ngành!' }]}
-            >
-              <Select placeholder="Chọn chuyên ngành">
-                {/* /// */}
-              </Select>
-            </Form.Item>
-
-            <Form.Item
-              label="Chuyên ngành hẹp"
-              name="narrowMajorCode"
-            >
-              <Select placeholder="Chọn chuyên ngành hẹp">
-                {/* /// */}
-              </Select>
-            </Form.Item>
-
-            <Form.Item
-              label="Trạng thái hoạt động"
-              name="isActive"
-              rules={[{ required: true, message: 'Vui lòng chọn trạng thái hoạt động!' }]}
-            >
-              <Radio.Group>
-                <Radio value={1}>Khả dụng</Radio>
-                <Radio value={0}>Tạm ẩn</Radio>
-              </Radio.Group>
-            </Form.Item>
+          <div className="form-group">
+            <label>Tên Môn Học:</label>
+            <input
+              type="text"
+              className="form-control"
+              {...register('subjectName', { required: 'Tên môn không được để trống.' })}
+            />
+            {errors.subjectName && <span className="text-danger">{errors.subjectName.message}</span>}
+          </div>
+          <div className="form-group">
+            <label>Số Tín Chỉ:</label>
+            <input
+              type="number"
+              className="form-control"
+              {...register('creditNumber', { required: 'Số tín chỉ không được để trống.', min: { value: 1, message: 'Số tín chỉ phải lớn hơn 0.' } })}
+            />
+            {errors.creditNumber && <span className="text-danger">{errors.creditNumber.message}</span>}
+          </div>
+          <div className="form-group">
+            <label>Mô Tả:</label>
+            <textarea
+              className="form-control"
+              {...register('description')}
+            />
+          </div>
+          <div className="form-group">
+            <label>Học Phí Môn:</label>
+            <input
+              type="number"
+              className="form-control"
+              {...register('tuition', { required: 'Học phí không được để trống.' })}
+            />
+            {errors.tuition && <span className="text-danger">{errors.tuition.message}</span>}
+          </div>
+          <div className="form-group">
+            <label>Học Phí Học Lại:</label>
+            <input
+              type="number"
+              className="form-control"
+              {...register('reStudyFee', { required: 'Học phí học lại không được để trống.' })}
+            />
+            {errors.reStudyFee && <span className="text-danger">{errors.reStudyFee.message}</span>}
+          </div>
+          <div className="form-group">
+            <label>Chuyên Ngành:</label>
+            <input
+              type="text"
+              className="form-control"
+              {...register('majorCode', { required: 'Chuyên ngành không được để trống.' })}
+            />
+            {errors.majorCode && <span className="text-danger">{errors.majorCode.message}</span>}
+          </div>
+          <div className="form-group">
+            <label>Kì Học:</label>
+            <input
+              type="text"
+              className="form-control"
+              {...register('semestersCode', { required: 'Kì học không được để trống.' })}
+            />
+            {errors.semestersCode && <span className="text-danger">{errors.semestersCode.message}</span>}
           </div>
         </div>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Thêm Môn học <i className='fas fa-plus' style={{ color: 'white' }}></i>
-          </Button>
-        </Form.Item>
-      </Form>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" onClick={handleClose}>Đóng</button>
+          <button type="submit" className="btn btn-primary">Thêm</button>
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
 export default AddSubject;
