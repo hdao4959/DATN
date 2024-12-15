8f8d37815173f542c9e09e26aaa5b5d34742fc41
hoanganhdzvcl <hoanganhdz2004@gmail.com>
2024-12-15 12:50:50 +0700

    update xem lịch , lớp học phía sinh viên

--- FE/src/config/axios.ts
+++ FE/src/config/axios.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://admin.feduvn.com/api/",
     // baseURL: "http://localhost:8000/api",
-
 });
 
 api.interceptors.request.use(
