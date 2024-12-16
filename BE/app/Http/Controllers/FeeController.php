29254e91a7015b034b9fbb98581d8fbba8f721d8
Trung <145466952+Trung2310@users.noreply.github.com>
2024-12-16 12:05:12 +0700

    Update FeeController.php

--- BE/app/Http/Controllers/FeeController.php
+++ BE/app/Http/Controllers/FeeController.php
@@ -25,20 +25,23 @@ class FeeController extends Controller
 
 
     public function index(Request $request)
-{
-    try {
-        $status = $request->input('status');
-        $email = $request->input('email');
-        $search = $request->input('search'); // Thêm điều kiện tìm kiếm
-
-        $data = $this->feeRepository->getAll($email, $status, $search);
-
-        return response()->json($data);
-    } catch (Throwable $th) {
-        return response()->json(['message' => $th->getMessage()], 404);
+    {
+        try {
+            $status = $request->input('status');
+            $email = $request->input('email');
+            $search = $request->input('search'); 
+            $orderBy = $request->input('orderBy', 'created_at'); // Nếu không có orderBy, mặc định là 'created_at'
+            $orderBy === 'user' ? ($orderBy = 'user_code') : $orderBy; 
+            $orderDirection = $request->input('orderDirection', 'asc'); // Mặc định sắp xếp theo hướng 'asc'
+    
+            $data = $this->feeRepository->getAll($email, $status, $search, $orderBy, $orderDirection);
+            
+    
+            return response()->json($data);
+        } catch (Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 404);
+        }
     }
-}
-
 
     /**
      * Show the form for creating a new resource.
