2c793daa92df1064f5a40c93b5c23144f53f6692
ryanhung044 <hunglsb2004@gmail.com>
2024-12-18 11:43:28 +0700

    Merge branch 'main' of https://github.com/hdao4959/DATN

--- FE/src/pages/student/Schedules/ViewSchedules.jsx
+++ FE/src/pages/student/Schedules/ViewSchedules.jsx
@@ -202,7 +202,7 @@ const ViewSchedules = () => {
 
             <div className="card">
                 <div className="card-header">
-                    <h4 className="card-title">Lịch học 7 ngày tới</h4>
+                    <h4 className="card-title">Lịch học</h4>
                 </div>
                 <div className="card-body">
                     <table
